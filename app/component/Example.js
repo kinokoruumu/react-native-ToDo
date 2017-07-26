3cc99bc70507268c17f77a3b2142d1a90edde2e0
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2017-07-27 04:32:58 +0900

    add Example

---
+++ app/component/Example.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import Swipeable from 'react-native-swipeable';
+import {
+    StyleSheet,
+    View,
+    Text,
+} from 'react-native'
+
+export default class Example extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      leftActionActivated: false,
+      toggle: false
+    }
+  }
+
+  render() {
+    const {leftActionActivated, toggle} = this.state;
+
+    return (
+      <Swipeable
+        leftActionActivationDistance={200}
+        leftContent={(
+          <View style={[styles.leftSwipeItem, {backgroundColor: leftActionActivated ? 'lightgoldenrodyellow' : 'steelblue'}]}>
+            {leftActionActivated ?
+              <Text>release!</Text> :
+              <Text>keep pulling!</Text>}
+          </View>
+        )}
+        onLeftActionActivate={() => this.setState({leftActionActivated: true})}
+        onLeftActionDeactivate={() => this.setState({leftActionActivated: false})}
+        onLeftActionComplete={() => this.setState({toggle: !toggle})}
+      >
+        <View style={[styles.listItem, {backgroundColor: toggle ? 'thistle' : 'darkseagreen'}]}>
+          <Text>Example 3</Text>
+        </View>
+      </Swipeable>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20
+  },
+  listItem: {
+    height: 75,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  leftSwipeItem: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingRight: 20
+  },
+  rightSwipeItem: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+
+});
